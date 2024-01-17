3ab178ecee198fde69406aaa62f83cbdfc115d08
Benjamin Galindo-Navarro <benjaming@microsoft.com>
2024-01-17 14:26:22 -0800

    Fix python build script finding spec file

--- GAUSS/LaunchDetectorScript/Build-Project.ps1
+++ GAUSS/LaunchDetectorScript/Build-Project.ps1
@@ -1,3 +1,9 @@
-# TODO: Freeze python project into an executable
 Write-Output "Building python project."
-pyinstaller .\LaunchDetectorScript.spec
+
+# Get the path of the script
+$scriptPath = $PSScriptRoot
+
+# Construct the path to the spec file
+$specFilePath = Join-Path -Path $scriptPath -ChildPath "LaunchDetectorScript.spec"
+
+pyinstaller $specFilePath
